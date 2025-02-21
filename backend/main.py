c5ababd816a141d575be92aa24379ed59f4e3657
mago <magomedovmarif2@gmail.com>
2025-02-21 15:19:07 +0300

    fix: cors

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(all_routers)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
