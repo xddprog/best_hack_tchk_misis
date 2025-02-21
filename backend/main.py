23aee773d0c7e20a7aad00b9fe2135846452927a
mago <magomedovmarif2@gmail.com>
2025-02-21 15:16:34 +0300

    fix: cors

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.api.v1.routers import all_routers
 from backend.infrastructure.database.adapters.pg_connection import DatabaseConnection
@@ -16,3 +17,10 @@ app = FastAPI(lifespan=lifespan)
 
 
 app.include_router(all_routers)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
