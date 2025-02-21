556881855d509d04113d43ebdb3dfb662f5fd3c8
flavokrkkk <145288995+flavokrkkk@users.noreply.github.com>
2025-02-21 14:21:15 +0300

    small fix

--- frontend/src/shared/api/baseQueryInstance.ts
+++ frontend/src/shared/api/baseQueryInstance.ts
@@ -3,6 +3,7 @@ import axios, {
   AxiosRequestConfig,
   AxiosResponse,
   AxiosError,
+  isAxiosError,
 } from "axios";
 import { RequestOptions } from "https";
 import tokenService from "@/entities/token/libs/tokenService";
@@ -23,6 +24,7 @@ export class AxiosClient {
 
     if (withAuth) {
       this.addAuthInterceptor();
+      // this.addAuthResponseInterceptor();
     }
   }
 
@@ -38,6 +40,63 @@ export class AxiosClient {
 
       return config;
     });
+  }
+
+  public addAuthResponseInterceptor() {
+    let isRefreshing = false;
+    let refreshSubscribers: ((token: string) => void)[] = [];
+
+    this.baseQueryV1Instance.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response?.status === 401 && !originalRequest._retry) {
+          originalRequest._retry = true;
+
+          if (isRefreshing) {
+            return new Promise((resolve) => {
+              refreshSubscribers.push((token: string) => {
+                originalRequest.headers.Authorization = `Bearer ${token}`;
+                resolve(this.baseQueryV1Instance(originalRequest));
+              });
+            });
+          }
+
+          isRefreshing = true;
+
+          try {
+            const { data } = await authService.refreshToken();
+
+            if (!data.access_token) throw new Error("Invalid refresh response");
+
+            tokenService.setAccessToken(data.access_token);
+            isRefreshing = false;
+
+            refreshSubscribers.forEach((callback) =>
+              callback(data.access_token)
+            );
+            refreshSubscribers = [];
+
+            originalRequest.headers.Authorization = `Bearer ${data.access_token}`;
+
+            return this.baseQueryV1Instance(originalRequest);
+          } catch (error) {
+            isRefreshing = false;
+            refreshSubscribers = [];
+            if (isAxiosError(error)) {
+              if (error.response?.status === 401) {
+                tokenService.deleteAccessToken();
+              }
+            }
+
+            return Promise.reject(error);
+          }
+        }
+
+        return Promise.reject(error);
+      }
+    );
   }
 
   private handleResponse<T>(response: AxiosResponse<T>): AxiosResponse<T> {
