d7130cc377defbead9b2c8e6763f87f6178aea10
flavokrkkk <145288995+flavokrkkk@users.noreply.github.com>
2025-02-21 22:51:06 +0300

    small update

--- frontend/src/pages/routes/routes.tsx
+++ frontend/src/pages/routes/routes.tsx
@@ -19,6 +19,10 @@ export const routes = createBrowserRouter([
     children: [
       ...routesWithHoc(privatePage, [
         {
+          path: "",
+          element: <Navigate to={ERouteNames.DEFAULT_ROUTE} replace />,
+        },
+        {
           path: ERouteNames.DEFAULT_ROUTE,
           element: <RootPage />,
           children: [
